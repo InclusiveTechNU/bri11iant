a7d19864c07314dbfd55177763ce4fa15276fb93
Cooper Barth <cooperfbarth@gmail.com>
2020-02-13 16:02:54 -0600

    fixed js

--- server/src/DOM.ts
+++ server/src/DOM.ts
@@ -4,6 +4,7 @@
 import { JSDOM } from "jsdom";
 
 export function createDOM(text: string, uri: string): Promise<JSDOM> {
+    // Create temporary DOM for initial parsing
     const tempDOM = new JSDOM(text);
 
     // Get absolute path to HTML file
@@ -20,16 +21,18 @@ export function createDOM(text: string, uri: string): Promise<JSDOM> {
     // Replace js scripts with full paths
     for (const link of tempDOM.window.document.querySelectorAll("script")) {
         const attribute = link.getAttribute("src");
-        if (attribute && attribute.charAt(0) !== "/") {
+        if (attribute && attribute.charAt(0) !== "/" && !/http*/i.test(attribute)) {
             link.setAttribute("src", uriPath + attribute);
         }
     }
     
+    // Create DOM and run scripts
 	const DOM = new JSDOM(tempDOM.serialize(), {
         resources: "usable",
         runScripts: "dangerously"
     });
     
+    // Return DOM when the scripts have loaded
     return new Promise(resolve => {
         DOM.window.addEventListener("load", () => {
             resolve(DOM);
